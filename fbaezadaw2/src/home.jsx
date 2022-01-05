ad054efc062543d179e828d2e0874b773b81f45c
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-01-05 23:39:42 +0100

    formulario

--- fbaezadaw2/src/home.jsx
+++ fbaezadaw2/src/home.jsx
@@ -2,7 +2,13 @@ import {useState, React} from 'react'
 
 export default function Dashboard(){
     //const [state, setstate]= useState(); Asi podemos usar los hooks de react
-    const [nota, setNota]= useState([
+    const initialNote ={
+        id:"",
+        title:"",
+        body:""
+    }
+
+    const [notas, setNotas]= useState([
         {id:1, title:'nota 1', body:'Hola'},
         {id:2, title:'nota 2', body:'Hi'},
         {id:3, title:'nota 3', body:'Bonjour'},
@@ -10,26 +16,62 @@ export default function Dashboard(){
         {id:5, title:'nota 5', body:'Ey'},
 
     ]);
+    const [note, setNote] = useState(initialNote);
+
     //Si quisiera modificar un hook, tengo que usar el setName para asi poder
     //hacer las modificaciones del mismo
-    const changeState=()=>{
-        const n1 = {id:6, title:'nota 6', body:'New change'};
-        setNota(nota.concat(n1));
+    // const changeState=()=>{
+    //     const n1 = {id:6, title:'nota 6', body:'New change'};
+    //     setNotas(notas.concat(n1));
+    // }
+    //   <button onClick={()=>changeState()}>Cambiar estado</button><br/>
+    const addNote=(ev)=>{  
+        ev.preventDefault();
+        console.log("submit");
+        //diferentes formas de mostrar las iteraciones de notas 
+        console.log(notas.map(note => note.id));
+        console.log(...notas.map(note => note.id));
+        //calcular el max del arrary para sacar el valor id del siguiente input
+        console.log(Math.max(...notas.map(note => note.id)));
+        console.log(Math.max(...notas.map(note => note.id))+1);
+
+
+        if(note.title == ""  || note.body=="") {return}
+        setNotas([
+            ...notas,
+            {
+                ...note,
+                id:Math.max(...notas.map(note => note.id))+1
+            }
+        ])
+
     }
     
     return (
         <div>
             <h1>Hola</h1>
-            <pre>{JSON.stringify(nota)}</pre>
+            <pre>{JSON.stringify(notas)}</pre>
             <h3>Listar un objeto</h3>
             <ul>
             {
-                nota.map(n=>{
-                    return <li>{n.id} -Titulo: {n.title} : {n.body}</li>;
+                notas.map(n=>{
+                    return <li key={n.id}>{n.id} -Titulo: {n.title} : {n.body}</li>;
                 })
             }
             </ul>
-            <button onClick={()=>changeState()}>Cambiar estado</button>
+            
+
+            <form onSubmit={(ev)=>addNote(ev)}>
+                <label htmlFor=''>Titulo
+                    <input id='title' type="text" onChange={ (ev)=>setNote({...note, title: ev.target.value})} />
+                </label><br/>
+                <label htmlFor=''>Accion
+                    <input id='body' type="text" onChange={ (ev)=>setNote({...note, body: ev.target.value})}/>
+                </label>
+                <button >Save</button>
+
+
+            </form>
 
 
         </div>
